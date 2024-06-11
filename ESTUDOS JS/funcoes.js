b939217a588e062abdc16aa9472297abac386087
kenkawabe <ek.kawabe01@gmail.com>
2024-06-10 23:29:27 -0300

    primeiro commit

---
+++ ESTUDOS JS/funcoes.js
@@ -0,0 +1,12 @@
+
+
+const sum = function( num1 , num2){
+      let total = num1 + num2;
+      return total
+}
+
+let num1 = 0
+let num2 = 200
+console.log('a some é ' + sum(num1 , num2))
+console.log(total)
+
---
+++ ESTUDOS JS/funcoes2.js
@@ -0,0 +1,11 @@
+function Person(name){
+   this.name = name
+}
+
+//const ken = new Person("ken")
+//const fly = new Person("fly")
+//console.log(ken)
+//console.log(fly)
+
+let date = new String("2002-08-09")
+console.log(date)
